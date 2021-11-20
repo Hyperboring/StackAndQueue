c927653ba32fd8a3e15417e2a93d66600f0cca9a
KatanaMajesty <sasha.panasenk@gmail.com>
2021-11-20 16:14:49 +0300

    stack finished, queue unfinished

--- main.cpp
+++ main.cpp
@@ -3,8 +3,14 @@
 
 using namespace std;
 
+/*
+ нужна ли функция суицида структуре?
+ как решить проблему с выводом данных структуры
+
+ */
+
 #define STACK_SIZE 6
-#define QUEUE_SIZE 6
+#define QUEUE_SIZE 4
 
 struct Book {
     char *name;
@@ -20,25 +26,32 @@ struct Stack {
 
     Stack() {
         size = STACK_SIZE;
-        arr = (Book **) (malloc(size * sizeof(Book *)));
+        arr = new Book*[size];
         for (int i = 0; i < size; i++)
             arr[i] = nullptr;
+    }
+
+    void printArr() const {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == nullptr) continue;
+            printf("%s %d %d\n", arr[i]->name, arr[i]->price, arr[i]->pages);
+        }
     }
 
     // explicit keyw запретит создание объекта с помощью Stack stack = 6;
     explicit Stack(unsigned int size) {
         this->size = size;
-        arr = (Book **) (malloc(size * sizeof(Book *)));
+        arr = new Book*[size];
         for (int i = 0; i < size; i++)
             arr[i] = nullptr;
     }
 
     // returns -0xFFF if stack is full
     // returns 1 if book was pushed into stack
-    int push(Book book) {
+    int push(Book *book) {
         if (top == size - 1)
             return -0xFFF;
-        arr[++top] = &book;
+        arr[++top] = book;
         return 1;
     }
 
@@ -81,50 +94,64 @@ struct Stack {
 
     // commit suicide
     void close() {
-//        delete[] arr;
-        free(arr);
+        delete[] arr;
+//        free(arr);
         delete this;
     }
+};
+
+struct Song {
+    char *name;
+    long length;
+    int likes;
 };
 
 struct Queue {
 
     int head = 0;
     int tail = 0;
-    int *arr;
+    Song *(*arr);
     unsigned int size;
 
     Queue() {
         size = QUEUE_SIZE;
-        arr = new int[size];
+        arr = new Song*[size];
         for (int i = 0; i < size; i++)
-            arr[i] = 0;
+            arr[i] = nullptr;
     }
 
     explicit Queue(unsigned int size) {
         this->size = size;
-        arr = new int[size];
+        arr = new Song*[size];
         for (int i = 0; i < size; i++)
-            arr[i] = 0;
+            arr[i] = nullptr;
     }
 
-    int push(int i) {
+    void printArr() const {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == nullptr) continue;
+            printf("%s %d %d\n", arr[i]->name, arr[i]->length, arr[i]->likes);
+        }
+    }
+
+    int push(Song *song) {
         if (getSize() == size)
             return -0xFFF;
-        arr[tail] = i;
+        arr[tail] = song;
         tail = (tail + 1) % (int) size;
-        return i;
+        return 1;
     }
 
-    int pop() {
+    // returns nullptr if queue is empty
+    Song* pop() {
         if (isEmpty())
-            return -0xFFF;
-        int i = arr[head];
+            return nullptr;
+        Song *s = arr[head];
         head = (head + 1) % (int) size;
-        return i;
+        return s;
     }
 
-    int getFront()
+    Song* getFront()
     const {
         return arr[head];
     }
@@ -133,7 +160,7 @@ struct Queue {
         int i = head > tail ? head : tail;
         int j = i == head ? tail : head;
         for (int k = i; k >= j; k--)
-            arr[k] = 0;
+            arr[k] = nullptr;
         int cl = i - j + 1;
         tail = 0;
         head = 0;
@@ -145,12 +172,13 @@ struct Queue {
         return head == tail;
     }
 
+
     int getSize()
     const {
         if (head > tail)
-            return (int) size - head + tail + 1;
+            return (int) size - head + tail;
         else
-            return tail - head + 1;
+            return tail - head;
     }
 
 };
@@ -161,29 +189,51 @@ int getRandPages() {
 
 int main() {
     srand(time(nullptr));
-    Stack books(6);
-    auto *ptr = &books;
-    /*
-     TODO 20.11
-    book_1book_2book_3book_4book_5book_5
-    x¤a
-     */
+//    Stack books(6);
+//    auto *ptr = &books;
+//    char* name;
+//    for (int i = 0; i < 5; i++) {
+//        name = new char[32];
+//        sprintf(name, "book_%d", i + 1);
+//        ptr->push(new Book{name, getRandPages(), 20*i});
+//    }
+//    ptr->printArr();
+//    printf("%s\n", ptr->pop()->name);
+//    printf("%s\n", ptr->pop()->name);
+//    for (int i = 5; i < 9; i++) {
+//        name = new char[32];
+//        sprintf(name, "book_%d", i + 1);
+//        ptr->push(new Book{name, getRandPages(), 20*i});
+//    }
+//    ptr->printArr();
+//    delete[] name;
+//    Book book = *(ptr->pop());
+//    book.pages -= 10;
+//    ptr->push(&book);
+//    for (int i = 0; i < 8; i++) {
+//        Book *b = ptr->pop();
+//        printf("%d\n", b == nullptr ? -0xFFF : b->price);
+//    }
+
+    printf("queue\n");
+    Queue queue(4);
+    auto *ptr_2 = &queue;
+    char *song_name;
+    for (int i = 0; i < 6; i++) {
+        song_name = new char[32];
+        sprintf(song_name, "song_%d", i + 1);
+        printf("%d\n", ptr_2->push(new Song{song_name, 1000 - (100 * i), 22 * i}));
+    }
+    ptr_2->printArr();
+    cout << "size: " << ptr_2->getSize() << endl;
+    Song song1 = *(ptr_2->pop());
+    Song song2 = *(ptr_2->pop());
+    printf("%d\n", ptr_2->push(&song1));
+    printf("%d\n", ptr_2->push(&song2));
+    Song s = *(ptr_2->pop());
+    s.likes += 100;
+    printf("%d\n", ptr_2->push(&s));
     for (int i = 0; i < 5; i++) {
-        char name[32];
-        sprintf(name, "book_%d", i + 1);
-        printf("%s", name);
-        ptr->push({name, getRandPages(), 20*i});
-    }
-    printf("%s\n", ptr->pop()->name);
-    printf("%s\n", ptr->pop()->name);
-    for (int i = 5; i < 9; i++) {
-        char name[32];
-        sprintf(name, "book_%d", i + 1);
-        printf("%d\n", ptr->push({name, getRandPages(), 20*i}));
-    }
-    ptr->pop()->pages -= 10;
-    for (int i = 0; i < 8; i++) {
-        Book *b = ptr->pop();
-        printf("%d\n", b == nullptr ? -0xFFF : b->price);
+        printf("%d\n", ptr_2->pop());
     }
 }
